4cb36a2d8e91090174e644937d85f3b0009e2aa6
Bjorn Hansson <bjorn@bjornhansson.se>
2014-09-21 22:45:50 +0200

    Some cleanup and demo

---
+++ README.md
@@ -0,0 +1,6 @@
+Argonbox
+========
+
+A really basic and lightweight lightbox plugin for jQuery. Easy to use, modify and expand.
+
+Live demo at [bjornhansson.se](http://bjornhansson.se/portfolio/jquery-plugin/)
--- js/jquery.argonbox.js
+++ js/jquery.argonbox.js
@@ -1,125 +1,131 @@
 /**
- * ArgonBox v1.0 - jQuery lightbox plugin
- * Björn Hansson - 2012
+ * ArgonBox - jQuery lightbox plugin
+ * Björn Hansson - 2013
  * License: http://licence.visualidiot.com/
  */
 (function($) {
-	$.fn.argonBox = function(options) {
-		options = $.extend({}, $.fn.argonBox.defaults, options);
-		
-		return this.each(function() {
-		
-			var windowHeigth = window.innerHeight || $(window).height(), // Make it work on iPad & Android
-				windowWidth  = window.innerWidth  || $(window).width(),
-				allLinks = $('.argonbox a'),
-				pos,
-				prev,
-				next,
-				
-				// (methods not finished yet)
-				methods = {
-					init : function( options ) {
-						// will be used later
-					},
-					show : function( ) {
-						// will be used later
-					},
-					close : function( ) { 
-						$('#overlay, #lightbox').remove();
-						$('body').css('overflow-y', 'auto'); // Show scrollbars
-						$('a').removeClass('current');
-					},
-					update : function( content ) {
-						// will be used later
-					}
-				};
-			// End of var declaration.
-			
-			
-			$('body').css('overflow-y', 'hidden'); // Hide scrollbars
-	
-			// Display the overlay
-			$('<div id="overlay"></div>')
-			.css('opacity', '0')
-			.animate({'opacity' : options.opacity}, options.duration)
-			.appendTo('body');
-	
-			// Create the lightbox container
-			$('<div id="lightbox"></div>')
-			.hide()
-			.appendTo('body');
-	
-			// Display the image on load
-			$('<img>')
-			.attr('src', $(this).attr('href'))
-			.css({
-				'max-height': windowHeigth-40, // -40, so that the description is visible.
-				'max-width':  windowWidth-40
-			})
-			.load(function() {
-				$('#lightbox')
-				.css({
-					'top':  (windowHeigth - $('#lightbox').height()) / 2,
-					'left': (windowWidth  - $('#lightbox').width())  / 2
-				})
-				.fadeIn();
-			})
-			.appendTo('#lightbox');
-			
-			// Create the description container
-			$('<div id="description"></div>')
-			.text($(this).attr('title'))
-			.appendTo('#lightbox');
-			
-			// Create the navigation container
-			if(options.bootstrap) { // If you want to use Bootstrap. (Bootstrap must of course be included in your site)
-				$('<div id="navigation"><a href="#" id="previous" class="btn btn-small"><i class="icon-arrow-left"></i></a> <a href="#" id="next" class="btn btn-small"><i class="icon-arrow-right"></i></a></div>')
-				.appendTo('#description');
-			}
-			else { // else use the standard buttons: www.glyphicons.com - http://creativecommons.org/licenses/by/3.0/
-				$('<div id="navigation"><a href="#" id="previous" class="previousBtn">&nbsp;</a> <a href="#" id="next" class="nextBtn">&nbsp;</a></div>')
-				.appendTo('#description');
-			}
-			
-			// Current link/image
-			$(this).addClass('current');
-			
-			// Previous link click
-			$('#previous').click(function() {
-				pos = allLinks.index($('a.current'));
-				prev = allLinks.get(pos-1); // Find previous from current
-				
-				methods.close();
+    "use strict";
 
-				prev.click(); // Make this current
-				return false;
-			});
-			
-			// Next link click
-			$('#next').click(function() {
-				pos = allLinks.index($('a.current'));
-				next = allLinks.get(pos+1);
-				
-				if(!next) {
-					next = allLinks.get(0);
-				}
-				methods.close();
+    $.fn.argonBox = function(options) {
+        options = $.extend({}, $.fn.argonBox.defaults, options);
+        
+        return this.each(function() {
+            var windowHeigth = window.innerHeight || $(window).height(), // Make it work on iPad & Android
+                windowWidth  = window.innerWidth  || $(window).width(),
+                allLinks = $('.argonbox a'),
+                currentPos,
+                prev,
+                next,
+                
+                methods = {
+                    init : function() {
+                        $('body').css('overflow-y', 'hidden'); // Hide scrollbars
+                
+                        // Display the overlay
+                        $('<div id="overlay"></div>')
+                        .css('opacity', '0')
+                        .animate({'opacity' : options.opacity}, options.duration)
+                        .appendTo('body');
 
-				next.click();
-				return false;
-			});
-	  
-			// Remove it all on click
-			$('#overlay, #lightbox').click(function() {
-				methods.close();
-			});
-		});
-	};
+                        // Create the lightbox container
+                        $('<div id="lightbox"></div>')
+                        .hide()
+                        .appendTo('body');
 
-	$.fn.argonBox.defaults = {
-		'duration': 'fast',
-		'opacity': '0.9',
-		'bootstrap': false,
-		'navigation': true,
-	}
+                        // Create the description container
+                        $('<div id="description"><strong></strong></div>').appendTo('#lightbox');
+                        
+                        if(allLinks.length > 1) {
+                            // Create the navigation container
+                            if(options.bootstrap) { // If you want to use buttons from Bootstrap. (Bootstrap must of course be included in your site)
+                                $('<div id="navigation"><a href="#" id="previous" class="btn btn-small"><span class="glyphicon glyphicon-chevron-left"></span></a> <a href="#" id="next" class="btn btn-small"><span class="glyphicon glyphicon-chevron-right"></span></a></div>')
+                                .appendTo('#description');
+                            }
+                            else { // else use the standard buttons: www.glyphicons.com - http://creativecommons.org/licenses/by/3.0/
+                                $('<div id="navigation"><a href="#" id="previous" class="previousBtn">&nbsp;</a> <a href="#" id="next" class="nextBtn">&nbsp;</a></div>')
+                                .appendTo('#description');
+                            }
+                        }
+                    },
+                    show : function( ) {
+                        $('#lightbox').hide();
+
+                        // Display the image on load
+                        $('<img>')
+                        .attr('src', $('a.current').attr('href'))
+                        .css({
+                            'max-height': windowHeigth-50, // -50, so that the description is visible.
+                            'max-width':  windowWidth-50
+                        })
+                        .load(function() {
+                            $('#lightbox')
+                            .css({
+                                'top':  (windowHeigth - $('#lightbox').height()) / 2,
+                                'left': (windowWidth  - $('#lightbox').width())  / 2
+                            })
+                            .show();
+                        })
+                        .prependTo('#lightbox');
+                        
+                        // Create the description container
+                        $('#description strong').text($('a.current').attr('title'));                    
+                    },
+                    close : function( ) {
+                        $('#overlay, #lightbox').remove();
+                        $('body').css('overflow-y', 'auto'); // Show scrollbars again
+                        $('a').removeClass('current');
+                    }
+                };
+            // End of var declaration.
+
+            // Set current link
+            $(this).addClass('current');
+            methods.init();
+            methods.show();
+            
+            // Previous link click
+            $('#previous').click(function() {
+                currentPos = allLinks.index($('a.current'));
+                prev = allLinks.get(currentPos-1); // Find previous from current
+
+                $('a').removeClass('current');
+                $(prev).addClass('current');
+
+                $('#lightbox img').remove();
+                methods.show();
+
+                return false;
+            });
+            
+            // Next link click
+            $('#next').click(function() {
+                currentPos = allLinks.index($('a.current'));
+                next = allLinks.get(currentPos + 1); // Find previous from current
+                
+                if(!next) {
+                    next = allLinks.get(0);
+                }
+
+                $('a').removeClass('current');
+                $(next).addClass('current');
+
+                $('#lightbox img').remove();
+                methods.show();
+
+                return false;
+            });
+      
+            // Remove it all on click
+            $('#overlay').click(function() {
+                methods.close();
+            });
+        });
+    };
+
+    $.fn.argonBox.defaults = {
+        'duration': 'fast',
+        'opacity': '0.9',
+        'bootstrap': false,
+        'navigation': true,
+    };
 }) (jQuery);
